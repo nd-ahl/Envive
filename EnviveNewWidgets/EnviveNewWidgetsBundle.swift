14de7c530591e6a74a40859ee55f9f67003ecfca
Neal <Nealahlstrom@gmail.com>
2025-09-29 17:40:56 -0600

    very basic camera functionality

---
+++ EnviveNewWidgets/EnviveNewWidgetsBundle.swift
@@ -0,0 +1,17 @@
+//
+//  EnviveNewWidgetsBundle.swift
+//  EnviveNewWidgets
+//
+//  Created by Paul Ahlstrom on 9/25/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct EnviveNewWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        EnviveNewWidgets()
+        EnviveNewWidgetsLiveActivity()
+    }
+}
